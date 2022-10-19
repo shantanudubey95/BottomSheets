2958a1493eddedfb966650005abd68725942d427
shantanudubey95 <109502475+shantanudubey95@users.noreply.github.com>
2022-10-19 11:37:52 +0530

    Added sample modals

--- App.tsx
+++ App.tsx
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ShowcaseApp } from "@gorhom/showcase-template";
+import React, { useMemo } from "react";
+import { StyleSheet } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { screens as defaultScreens } from "./app/src/screens";
+import { version } from "./package.json";
+
+const author = {
+  username: "Mo Gorhom",
+  url: "https://gorhom.dev",
+};
+
+interface AppProps {
+  screens?: any[];
 }
+
+const App = ({ screens: providedScreens }: AppProps) => {
+  const screens = useMemo(
+    () => [...defaultScreens, ...(providedScreens ? providedScreens : [])],
+    [providedScreens]
+  );
+  return (
+    <GestureHandlerRootView style={styles.container}>
+      <ShowcaseApp
+        name="Bottom Sheet"
+        description=""
+        version={version}
+        author={author}
+        data={screens}
+      />
+      {/* <View>
+        <Text>Hello</Text>
+      </View> */}
+    </GestureHandlerRootView>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexGrow: 1,
   },
 });
+
+export default App;
---
+++ index.ts
@@ -0,0 +1,10 @@
+export { default as ModalBackdropExample } from "./app/src/screens/modal/BackdropExample";
+export { withModalProvider } from "./app/src/screens/modal/withModalProvider";
+export { Button } from "./app/src/components/button";
+export { ContactList } from "./app/src/components/contactList";
+export { ContactItem } from "./app/src/components/contactItem";
+export {
+  SearchHandle,
+  SEARCH_HANDLE_HEIGHT,
+} from "./app/src/components/searchHandle";
+export * from "./app/src/utilities/createMockData";
